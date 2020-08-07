23a46f75d0cab0dd15a930eb55eed2f15849ba2c
Egor <jux7ent@yandex.ru>
2020-08-07 16:00:00 +0300

    added simple ui for inventory

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -6,6 +6,15 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float _movementSpeed = 1f;
     [SerializeField] private float _minZPos = -1f;
     [SerializeField] private float _maxZPos = 1f;
+    [SerializeField] private float _throwRange = 10f;
+
+    private void Update() {
+        Throw();
+    }
+
+    public void Throw() {
+        Debug.DrawLine(transform.position + transform.forward * _throwRange, Vector3.down * 10f);
+    }
 
     public void MoveLeft() {
         MoveDirection(-1);
